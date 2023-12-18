b6c1b6aa19271be77f8e1e78bed31fce2f4f5e2f
sunilteja00 <147476599+sunilteja00@users.noreply.github.com>
2023-12-18 14:12:01 +0530

    #include<stdio.h> int main() {     char ch;     int i,j,n;     scanf("%d",&n);     for(i=65;i<=64+n;i++)     {         for(j=65;j<=64+n;j++)         {             printf("%c ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=65;i<=64+n;i++)
+    {
+        for(j=65;j<=64+n;j++)
+        {
+            printf("%c ",i);
+        }
+        printf("
+");
+    }
+}
