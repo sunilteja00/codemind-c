54ba6f190040c5c56edc29a86ec74b894078b976
sunilteja00 <147476599+sunilteja00@users.noreply.github.com>
2023-12-19 10:46:11 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int i,arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)        {             sum=sum+arr[i];        }     }
        printf("%d",sum);
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int i,arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0)
+       {  
+            sum=sum+arr[i];
+       }
+    }
+   
+    printf("%d",sum);
+}
